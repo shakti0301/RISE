eb153615ea18d43369a012e7c13e48d979a17a28
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-12 23:32:42 +0530

    Make Csharp folder and add project 1 in it

---
+++ Csharp/Project1/Project1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstCSharpApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Shaktisinh!");
+            Console.WriteLine("My first C# program");
+        }
+    }
+}
