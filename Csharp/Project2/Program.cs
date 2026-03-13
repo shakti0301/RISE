40c17f08714553a6a72367d421b16ce40a67359a
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-13 14:59:32 +0530

    Make changes in Csharp

---
+++ Csharp/Project2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static unsafe void Main()
+        {
+            // Basic Data Types
+            int number = 10;
+            float temperature = 36.5f;
+            char grade = 'A';
+            bool isStudent = true;
+            double price = 99.99;
+
+            Console.WriteLine("Integer: " + number);
+            Console.WriteLine("Float: " + temperature);
+            Console.WriteLine("Char: " + grade);
+            Console.WriteLine("Boolean: " + isStudent);
+            Console.WriteLine("Double: " + price);
+
+            Console.WriteLine("\n--- Pointer Example ---");
+
+            int value = 50;
+
+            // Pointer declaration
+            int* ptr = &value;
+
+            Console.WriteLine("Value of variable: " + value);
+            Console.WriteLine("Address of variable: " + (long)ptr);
+            Console.WriteLine("Value using pointer: " + *ptr);
+        }
+    }
+}
