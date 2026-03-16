556eb2a01a9ee61d1ec76dff47ad830198da64c1
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-16 12:55:01 +0530

    Make JavaScript folder and add files in it

---
+++ JavaScript/Practices/script.js
@@ -0,0 +1,41 @@
+console.log('Hello, World! This is a simple JavaScript file.');
+
+let firstname = 'suresh';
+console.log(firstname);
+
+let firstname1 = 'suresh';
+let lastname1 = 'sharma';
+console.log(firstname1 + ' ' + lastname1);
+console.log(firstname1, lastname1);
+console.log(firstname1);
+console.log(lastname1);
+
+// DATATYPE //
+
+let year = 20;
+let firstname2 = 'ramesh';
+let age = 20.5;
+console.log(age);
+console.log(age);
+console.log(age);
+
+let javaisfun = true;
+console.log(typeof javaisfun);
+
+let undefinedVariable;
+console.log(undefinedVariable);
+
+let agesuresh = 2026 - 1990;
+let agesurewh = 1990 - 2026;
+let ageramesh = 2026 - 1995;
+console.log(agesuresh);
+console.log(agesurewh);
+console.log(ageramesh);
+
+console.log(agesuresh * 2, agesuresh / 2, agesuresh ** 2);
+
+let x = 10 + 5;
+x += 10; // x= x+10
+x++;
+x--;
+console.log(x);
