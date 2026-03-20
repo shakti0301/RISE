030f57c86e1b0a0ea09079acd69a38314ffbc64d
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-20 14:28:06 +0530

    Make changes in file - StaticReadonly

--- Csharp/StaticAndReadonlyDemo/Program.cs
+++ Csharp/StaticAndReadonlyDemo/Program.cs
@@ -3,56 +3,55 @@
 namespace StaticAndReadonltDemo
 {
     static class Utility
-{
-    public static void Show()
     {
-        Console.WriteLine("Utility class");
-    }
-}
-
-class Demo
-{
-    public static int staticVar = 10;
-    public int normalVar = 5;
-
-    public const double PI = 3.14;
-    public readonly int value;
-
-    static Demo()
-    {
-        Console.WriteLine("Static constructor called");
+        public static void Show()
+        {
+            Console.WriteLine("Utility class");
+        }
     }
 
-    public Demo(int v)
+    class Demo
     {
-        value = v;
+        public static int staticVar = 10;
+        public int normalVar = 5;
+
+        public const double PI = 3.14;
+        public readonly int value;
+
+        static Demo()
+        {
+            Console.WriteLine("Static constructor called");
+        }
+
+        public Demo(int v)
+        {
+            value = v;
+        }
+
+        public void NormalMethod()
+        {
+            Console.WriteLine(staticVar); // allowed
+            Console.WriteLine(normalVar); // allowed
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine(staticVar); // allowed
+            // Console.WriteLine(normalVar); // not allowed
+        }
     }
 
-    public void NormalMethod()
+    class Program
     {
-        Console.WriteLine(staticVar); // allowed
-        Console.WriteLine(normalVar); // allowed
+        static void Main()
+        {
+            Demo.StaticMethod();
+
+            Demo obj = new Demo(100);
+            obj.NormalMethod();
+
+            Console.WriteLine(Demo.PI);
+            Utility.Show();
+        }
     }
-
-    public static void StaticMethod()
-    {
-        Console.WriteLine(staticVar); // allowed
-        // Console.WriteLine(normalVar); // not allowed
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        Demo.StaticMethod();
-
-        Demo obj = new Demo(100);
-        obj.NormalMethod();
-
-        Console.WriteLine(Demo.PI);
-        Utility.Show();
-    }
-}
-    
 }
