f63c793d8ce3b20ec43d0e263a4dd931fbe85668
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-24 16:52:21 +0530

    Make files

--- JavaScript/Practices/23-promise.js
+++ JavaScript/Practices/23-promise.js
@@ -1,11 +1,27 @@
-const fetchData = new Promise((resolve, reject) => {
-  let success = false;
+// const fetchData = new Promise((resolve, reject) => {
+//   //   let success = true;
+//   let success = Math.random() > 0.5;
 
-  if (success) {
-    resolve('Data fatched successfully');
-  } else {
-    reject('Error while fetching data');
-  }
-});
+//   setTimeout(() => {
+//     if (success) {
+//       resolve('Data fatched successfully');
+//     } else {
+//       reject('Error while fetching data');
+//     }
+//   }, 5000);
+// });
 
-fetchData.then((res) => console.log(res)).catch((err) => console.log(err));
+// fetchData.then((res) => console.log(res)).catch((err) => console.log(err));
+
+function getUsers() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Promise Data:', data);
+    })
+    .catch((err) => {
+      console.log('Promise Error:', err);
+    });
+}
+
+getUsers();
