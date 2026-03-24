9efeb17c7c76ec7c9ad0c02626871eba85deb8d1
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-24 22:25:29 +0530

    Add Inheritance app in to OOPS folder of Csharp

---
+++ Csharp/OOPS/InheritanceDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Base class (Parent)
+class Vehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Vehicle started");
+    }
+}
+
+// HAS-A class
+class Engine
+{
+    public void EngineStart()
+    {
+        Console.WriteLine("Engine is running");
+    }
+}
+
+// Child class (Inheritance + Composition)
+class Car : Vehicle   // IS-A relationship
+{
+    private Engine engine = new Engine();  // HAS-A relationship
+
+    public void Drive()
+    {
+        engine.EngineStart();  // using Engine
+        Console.WriteLine("Car is driving");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car c = new Car();
+
+        c.Start();   // inherited (IS-A)
+        c.Drive();   // own + HAS-A
+    }
+}
