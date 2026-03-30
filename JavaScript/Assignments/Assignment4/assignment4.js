6d7867f44f1fcd7b842d5fa63d7dc931ffcf632c
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-30 23:14:27 +0530

    assignment 4 edited

--- JavaScript/Assignments/Assignment4/assignment4.js
+++ JavaScript/Assignments/Assignment4/assignment4.js
@@ -1,7 +1,7 @@
 // 1. Employee Salary Processor
 const Employee = [
   { name: 'Shakti', salary: 41000 },
-  { name: 'Kushal', salary: 45000 },
+  { name: 'Kushal', salary: 75000 },
   { name: 'Shyam', salary: 10000 },
 ];
 
@@ -10,8 +10,67 @@ const filteredEmployees = Employee.filter(
 );
 
 const increasedSalary = filteredEmployees.map((employee) => {
-  const incrementedSalary = employee.salary + employee.salary * 0.1;
-  return { ...employee, incrementedSalary };
+  const incrementedSalary = employee.salary * 0.1;
+  const finalSalary = employee.salary + incrementedSalary;
+  return { ...employee, finalSalary };
 });
 
-console.log(increasedSalary);
+const employeeLevel = increasedSalary.map((employee) => {
+  let level;
+  if (employee.finalSalary >= 70000) {
+    level = 'Senior';
+  } else {
+    level = 'Mid';
+  }
+
+  return { ...employee, level };
+});
+
+console.log(employeeLevel);
+
+// 2. Order Management System
+
+const orders = [
+  {
+    orderId: 1,
+    customerName: 'Shakti',
+    items: [
+      { itemName: 'Laptop', quantity: 1, price: 1000 },
+      { itemName: 'Mouse', quantity: 2, price: 500 },
+    ],
+  },
+  {
+    orderId: 2,
+    customerName: 'Kushal',
+    items: [
+      { itemName: 'Keyboard', quantity: 1, price: 1000 },
+      { itemName: 'Mouse', quantity: 1, price: 700 },
+      { itemName: 'Bag', quantity: 1, price: 3500 },
+    ],
+  },
+  {
+    orderId: 3,
+    customerName: 'Shyam',
+    items: [
+      { itemName: 'iPhone', quantity: 1, price: 90000 },
+      { itemName: 'Laptop Bag', quantity: 3, price: 2000 },
+    ],
+  },
+];
+const result = orders.map((order) => {
+  let totalAmount = 0;
+  let totalQuantity = 0;
+
+  order.items.forEach((item) => {
+    totalAmount += item.price * item.quantity;
+    totalQuantity += item.quantity;
+  });
+
+  return {
+    orderId: order.orderId,
+    totalAmount: totalAmount,
+    totalQuantity: totalQuantity,
+  };
+});
+
+console.log(result);
