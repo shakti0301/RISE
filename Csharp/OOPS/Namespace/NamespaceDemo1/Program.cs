ad5c4f61da83737b94f33bce6f536b670cfdaae5
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-31 23:39:13 +0530

    Edit files of NamespaceDemo1

--- Csharp/OOPS/Namespace/NamespaceDemo1/Program.cs
+++ Csharp/OOPS/Namespace/NamespaceDemo1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Demo2; //import namespace
+using tt = Demo3.Demo4;  //alias
 class Program
 {
     static void Main()
@@ -7,7 +8,15 @@ class Program
         Demo1.A a = new Demo1.A();  //using full name (namespace.classname)
         a.show();
 
+        Demo1.A.show2();  //Static method in Demo1 namespace
+
         B b = new B();  //demo 2 is already imported by 'using Demo2;' above here we dont need to write full name
         b.display();
+
+        tt.C c = new tt.C();  //nested namespace 
+        c.show3();
+
+        Demo5.Outer.Inner obj = new Demo5.Outer.Inner();
+        obj.Show();
     }
 }
--- Csharp/OOPS/Namespace/NamespaceDemo1/Program2.cs
+++ Csharp/OOPS/Namespace/NamespaceDemo1/Program2.cs
@@ -6,6 +6,10 @@ namespace Demo1
         {
             Console.WriteLine("Inside Demo 1 - class A");
         }
+        public static void show2()
+        {
+            Console.WriteLine("Inside Demo 1 - class A - Static Method"); //for that no need to create an object, we can anle to call by class name
+        }
     }
 }
 
@@ -16,6 +20,37 @@ namespace Demo2
         public void display()
         {
             Console.WriteLine("Inside Demo 2 - Class B");
+        }
+    }
+}
+
+//Namespace inside Namespace
+namespace Demo3
+{
+    namespace Demo4
+    {
+        class C
+        {
+            public void show3()
+            {
+                Console.WriteLine("Nested Namespace Demo3.Demo4");
+            }
+        }
+    }
+}
+
+
+//Nested
+namespace Demo5
+{
+    class Outer
+    {
+        public class Inner
+        {
+            public void Show()
+            {
+                Console.WriteLine("Nested Class");
+            }
         }
     }
 }
