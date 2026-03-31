b0bc9912d68ea006de6409b9e06e9fb7a5060571
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-31 22:50:04 +0530

    Add Namespace Demo 1 in Csharp-OOPs-Namespace

---
+++ Csharp/OOPS/Namespace/NamespaceDemo1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Demo2; //import namespace
+class Program
+{
+    static void Main()
+    {
+        Demo1.A a = new Demo1.A();  //using full name (namespace.classname)
+        a.show();
+
+        B b = new B();  //demo 2 is already imported by 'using Demo2;' above here we dont need to write full name
+        b.display();
+    }
+}
---
+++ Csharp/OOPS/Namespace/NamespaceDemo1/Program2.cs
@@ -0,0 +1,21 @@
+namespace Demo1
+{
+    class A
+    {
+        public void show()
+        {
+            Console.WriteLine("Inside Demo 1 - class A");
+        }
+    }
+}
+
+namespace Demo2
+{
+    class B
+    {
+        public void display()
+        {
+            Console.WriteLine("Inside Demo 2 - Class B");
+        }
+    }
+}
