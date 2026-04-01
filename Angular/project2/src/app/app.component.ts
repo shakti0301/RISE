a233366c129102843e7e9b3a570ac2c69c2a2365
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-01 17:55:52 +0530

    Project2 add

---
+++ Angular/project2/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'project2';
+
+  showText = false;
+  off = true;
+
+  showSome() {
+    this.showText = !this.showText;
+    this.off = !this.off;
+  }
+  title2 = 'Hello World';
+}
