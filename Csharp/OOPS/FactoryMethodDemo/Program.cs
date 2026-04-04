eed00d52ebee25acc58f95cfa47a7fae8745b568
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-04 14:42:32 +0530

    Added factorymethod's program in Csharp - OOPS

---
+++ Csharp/OOPS/FactoryMethodDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FactoryMethodDemo
+{
+    interface INotification
+    {
+        void send();
+    }
+    
+    class EmailNotification : INotification
+    {
+        public void send()
+        {
+            Console.WriteLine("Email Notification sent successfully");
+        }
+    }
+
+    class SmsNotification : INotification
+    {
+        public void send()
+        {
+            Console.WriteLine("SMS Notification sent successfully");
+        }
+    }
+    class PhNotification : INotification
+    {
+        public void send()
+        {
+            Console.WriteLine("Phone Notification sent");
+        }
+    }
+
+    class NotificationFactory
+    {
+        public static INotification CreateNotification(String type)
+        {
+            if(type == "email")
+                return new EmailNotification();
+            else if (type == "sms")
+                return new SmsNotification();
+            else if (type == "phone")
+                return new PhNotification();
+            else
+                throw new Exception("Invalid input");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            INotification notification;
+
+            notification = NotificationFactory.CreateNotification("sms");
+            notification.send();
+        }
+    }
+}
