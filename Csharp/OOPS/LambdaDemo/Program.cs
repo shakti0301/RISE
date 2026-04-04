0b44b4fa2fc6d123f06d5bdcb92d81a90fa8e7d4
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-04 16:59:59 +0530

    Add Lambda demo in to Csharp - OOPs

---
+++ Csharp/OOPS/LambdaDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LambdaDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the value of A : ");
+            int a = int.Parse(Console.ReadLine() ?? "0");
+
+            Console.Write("Enter the value of B : ");
+            int b = int.Parse(Console.ReadLine() ?? "0");
+
+            // Basic lambda expression using Func<in, in, out>
+            Func<int, int, int> sum = (x, y) => x + y;
+
+            int result = sum(a, b);
+            Console.WriteLine("Sum of A and B is : " + result);
+        }
+    }
+}
