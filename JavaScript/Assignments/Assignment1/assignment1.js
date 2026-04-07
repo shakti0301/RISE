ddd05def6afddaa1ca8ccdbdd0df9c3f1098453b
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-07 09:24:45 +0530

    Change folder structure

--- JavaScript/Assignments/Assignment1/assignment1.js
+++ JavaScript/Assignments/Assignment1/assignment1.js
@@ -1,15 +1,99 @@
-let sureshHeight = 1.71;
-let rameshHeight = 1.76;
+//Assignment 1: Smart Discount Calculator
 
-let sureshMass = 60;
-let rameshMass = 75;
+let name = 'Shaktisinh';
+let age = 22;
+let purchaseAmount = 12000;
+let discount;
 
-let sureshBMI = sureshMass / sureshHeight ** 2;
-let rameshBMI = rameshMass / rameshHeight ** 2;
+if (age < 18) {
+  discount = 0.1;
+} else if (age >= 60) {
+  discount = 0.2;
+} else {
+  discount = 0;
+}
 
-console.log(`BMI of Suresh is ${sureshBMI}`);
-console.log(`BMI of Ramesh is ${rameshBMI}`);
+if (purchaseAmount > 5000) {
+  discount = discount + 0.05;
+}
 
-let markHigherBMI = sureshBMI > rameshBMI;
+let finalAmount = purchaseAmount - purchaseAmount * discount;
 
-console.log(`Suresh has higher BMI than Ramesh : ${markHigherBMI}`);
+console.log(
+  `Hello ${name}, your final amount after discount is: ${finalAmount}`,
+);
+
+//----------------------------------------------------------------
+//Assignment 2: Login Validation System
+
+let actualUsername = 'shakti0301';
+let actualPassword = 'pass12345';
+
+let enteredUsername = 'shakti0301';
+let enteredPassword = 'pass12345';
+
+if (enteredUsername != actualUsername) {
+  if (enteredPassword != actualPassword) {
+    console.log('Both Username and Password is wrong');
+  } else {
+    console.log('Your Username OR Password is worng');
+  }
+} else if (enteredPassword != actualPassword) {
+  console.log('Incorrect Password');
+} else {
+  console.log('Login Successful!');
+}
+
+//---------------------------------------------------------------
+//Assignment 3: Number Analyzer Tool
+
+let number = -4;
+
+if (number === 0) {
+  console.log('Entered number is zero');
+} else if (number > 0) {
+  if (number % 2 == 0) {
+    console.log(`The number ${number} is positive and even.`);
+  } else {
+    console.log(`The number ${number} is positive and odd.`);
+  }
+} else if (number % 2 == 0) {
+  console.log(`The number ${number} is negative and even.`);
+} else {
+  console.log(`The number ${number} is negative and odd.`);
+}
+
+//---------------------------------------------------------------
+//Assignment 4: Shopping Eligibility Checker
+let isMember = false;
+let cartValue = 2200;
+
+if ((isMember && cartValue > 1000) || cartValue > 2000) {
+  console.log('You are eligible for free delivery!');
+} else {
+  console.log('You are not eligible for free delivery!');
+}
+
+//---------------------------------------------------------------
+//Assignment 5: Grade Evaluation System
+
+let marks = 40;
+let Grade;
+
+if (marks > 100 || marks < 0) {
+  console.log('Invalid Input');
+} else {
+  if (marks >= 90) {
+    Grade = 'A';
+  } else if (marks <= 89 && marks >= 75) {
+    Grade = 'B';
+  } else if (marks <= 74 && marks >= 50) {
+    Grade = 'C';
+  } else {
+    Grade = 'F (Fail)';
+  }
+}
+
+console.log(`Your Grade is ${Grade}`);
+
+//change
--- JavaScript/Assignments/Assignment3/assignment3.js
+++ JavaScript/Assignments/Assignment3/assignment3.js
@@ -1,95 +1,76 @@
-// 1: Student Report System
-
-const Students = [
-  { name: 'Shakti', marks: [90, 70, 50, 60, 100] },
-  { name: 'Kushal', marks: [80, 78, 67, 45, 90] },
-  { name: 'Shyam', marks: [10, 50, 90, 30, 20] },
+// 1. Employee Salary Processor
+const Employee = [
+  { name: 'Shakti', salary: 41000 },
+  { name: 'Kushal', salary: 75000 },
+  { name: 'Shyam', salary: 10000 },
 ];
 
-const studentsWithAverage = Students.map((student) => {
-  const totalMarks = student.marks.reduce((num, res) => num + res, 0);
-  const averageMarks = totalMarks / student.marks.length;
-  return { ...student, average: averageMarks };
+const filteredEmployees = Employee.filter(
+  (employee) => employee.salary > 40000,
+);
+
+const increasedSalary = filteredEmployees.map((employee) => {
+  const incrementedSalary = employee.salary * 0.1;
+  const finalSalary = employee.salary + incrementedSalary;
+  return { ...employee, finalSalary };
 });
 
-const studentsWithResult = studentsWithAverage.map((student) => {
-  let result;
-  if (student.average >= 75) {
-    result = 'Distinction';
-  } else if (student.average >= 50) {
-    result = 'Pass';
+const employeeLevel = increasedSalary.map((employee) => {
+  let level;
+  if (employee.finalSalary >= 70000) {
+    level = 'Senior';
   } else {
-    result = 'Fail';
+    level = 'Mid';
   }
-  return { ...student, result };
+
+  return { ...employee, level };
 });
 
-console.log(studentsWithResult);
+console.log(employeeLevel);
 
-//2: Shopping Cart Analyzer
+// 2. Order Management System
 
-const items = [
-  { name: 'Laptop', price: 1000, quantity: 2 },
-  { name: 'Phone', price: 500, quantity: 3 },
-  { name: 'Headphones', price: 100, quantity: 5 },
+const orders = [
+  {
+    orderId: 1,
+    customerName: 'Shakti',
+    items: [
+      { itemName: 'Laptop', quantity: 1, price: 1000 },
+      { itemName: 'Mouse', quantity: 2, price: 500 },
+    ],
+  },
+  {
+    orderId: 2,
+    customerName: 'Kushal',
+    items: [
+      { itemName: 'Keyboard', quantity: 1, price: 1000 },
+      { itemName: 'Mouse', quantity: 1, price: 700 },
+      { itemName: 'Bag', quantity: 1, price: 3500 },
+    ],
+  },
+  {
+    orderId: 3,
+    customerName: 'Shyam',
+    items: [
+      { itemName: 'iPhone', quantity: 1, price: 90000 },
+      { itemName: 'Laptop Bag', quantity: 3, price: 2000 },
+    ],
+  },
 ];
-
-function calculateTotal(...items) {
+const result = orders.map((order) => {
   let totalAmount = 0;
   let totalQuantity = 0;
-  totalAmount = items.reduce(
-    (total, item) => total + item.price * item.quantity,
-    0,
-  );
-  totalQuantity = items.reduce((total, item) => total + item.quantity, 0);
-  return { totalAmount, totalQuantity };
-}
 
-const total = calculateTotal(...items);
-console.log(total);
+  order.items.forEach((item) => {
+    totalAmount += item.price * item.quantity;
+    totalQuantity += item.quantity;
+  });
 
-// 3: User Profile Updater
-const userProfile = {
-  name: 'Shakti',
-  age: 22,
-  city: 'Anand',
-};
-
-function updateUsrProfile(newCity, profession) {
-  return { ...userProfile, city: newCity, profession };
-}
-
-const updatedProfile = updateUsrProfile('Ahmedabad', 'Developer');
-console.log(updatedProfile);
-console.log(userProfile);
-
-//4: Product Filter & Transform
-const products = [
-  { name: 'Laptop', price: 1500 },
-  { name: 'Phone', price: 800 },
-  { name: 'Headphones', price: 1200 },
-];
-
-const filteredProducts = products.filter((product) => product.price > 1000);
-
-const discountedProducts = filteredProducts.map((product) => {
-  const discountedPrice = product.price - product.price * 0.1;
-  return { ...product, discountedPrice };
+  return {
+    orderId: order.orderId,
+    totalAmount: totalAmount,
+    totalQuantity: totalQuantity,
+  };
 });
 
-console.log(discountedProducts);
-
-//5: Dynamic Number Processor
-const numbers = [23, 12, 34, 45, 56, 67, 78, 89, 90];
-function dunamicNumberProcessor(...numbers) {
-  const evenNumbers = numbers.filter((num) => num % 2 === 0);
-  const oddNumbers = numbers.filter((num) => num % 2 !== 0);
-
-  const sumEven = evenNumbers.reduce((total, num) => total + num, 0);
-  const sumOdd = oddNumbers.reduce((total, num) => total + num, 0);
-
-  return { evenNumbers, oddNumbers, sumEven, sumOdd };
-}
-
-const result = dunamicNumberProcessor(...numbers);
 console.log(result);
