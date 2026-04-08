191d674f6b7a1b366cae4c2a0bf49cd2cd951da5
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-08 11:10:47 +0530

    Add PredicateDelegate in Csharp OOPs

---
+++ Csharp/OOPS/29-PredicateDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PredicatDelegate
+{
+    class Program
+    {
+        //Predicate is the delegate like Func and Action delegates. It represents a method containing a set of criteria and checks whether the passed parameter meets those criteria. A predicate delegate methods must take one input parameter and return a boolean - true or false.
+        static bool CheckIsUpperCase(string s)
+        {
+            return s.Equals(s.ToUpper());
+        }
+        static bool CheckIsLowerCase(string s)
+        {
+            return s.Equals(s.ToLower());
+        }
+        static void Main()
+        {
+            Predicate<string> isUpper = CheckIsUpperCase;
+            bool result1 = isUpper("hi");
+            Console.WriteLine("Result for isUpper is "+result1);
+
+            Predicate<string> isLower = CheckIsLowerCase;
+            bool result2 = isLower("hi");
+            Console.WriteLine("Result for isLower is "+result2);
+
+
+            //Predicate can also be used with any method, anonymous method, or lambda expression.
+            int y = 10;
+
+
+            //anonymous method
+            // Predicate<int> isGrater = delegate(int num)
+            // {
+            //     return num.CompareTo(y) > 0;
+            // };
+
+            // bool result3 = isGrater(2);
+            // Console.WriteLine(result3);
+
+            //lambda expression
+            Predicate<int> isGrater = (num) => num.CompareTo(y) > 0;
+            bool result4 = isGrater(1);
+            Console.WriteLine(result4);
+
+        }
+    }
+}
