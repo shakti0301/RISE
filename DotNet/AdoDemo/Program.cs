44bc31fad9d7723f3291d02cddbfc19456ceca37
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-14 17:34:11 +0530

    Create a new folder for .net - currently adding files of ADO.NET

---
+++ DotNet/AdoDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Read connection string from appsettings.json
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        IConfiguration config = builder.Build();
+        string connStr = config.GetConnectionString("MyConn");
+
+        // 2. using block (auto close connection)
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+            conn.Open();
+            Console.WriteLine("Connected to DB\n");
+
+            // 3. SQL Query
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Students", conn);
+
+            // 4. ExecuteReader (for multiple rows)
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            // 5. Read data
+            Console.WriteLine("Student Data:\n");
+
+            while (reader.Read())
+            {
+                Console.WriteLine(
+                    reader["Id"] + " - " +
+                    reader["Name"] + " - " +
+                    reader["Age"]
+                );
+            }
+
+            reader.Close();
+        }
+    }
+}
