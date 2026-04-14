dc26338cb4bfea7dd277fbf3f8fec09cffadaa98
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-14 10:10:35 +0530

    Add ObjectInitializer in Csharp - AdvancedConcepts

---
+++ Csharp/AdvancedConcepts/33-ObjectInitializer/Program.cs
@@ -0,0 +1,27 @@
+﻿//Object Initializer allows you to create an object and assign values at the same time without writing a constructor manually.
+
+using System;
+
+namespace ObjectInitializer
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        // Cleaner code
+        // No need for constructor
+        // Easy initialization
+        static void Main()
+        {
+            Student s1 = new Student()
+            {
+              Name = "Shakti",
+              Age = 21  
+            };
+            Console.WriteLine($"Your name is {s1.Name} and Age is {s1.Age}");
+        }
+    }
+}
