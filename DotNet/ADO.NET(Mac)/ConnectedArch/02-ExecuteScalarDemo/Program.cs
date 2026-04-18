1d21acc1d9736a54544932c4ea0b615ac73cd9d8
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-18 19:15:51 +0530

    Folder Structure changed

---
+++ DotNet/ADO.NET(Mac)/ConnectedArch/02-ExecuteScalarDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main()
+    {
+        var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+        IConfiguration config = builder.Build();
+        string connStr = config.GetConnectionString("MyConn");
+
+        using(SqlConnection conn = new SqlConnection(connStr))
+        {
+            conn.Open();
+            Console.WriteLine("\nConnection to DB - Success");
+
+            SqlCommand countCmd = new SqlCommand("SELECT COUNT(Id) FROM Students", conn);
+            int count = (int)countCmd.ExecuteScalar();
+            Console.WriteLine("Total Students: " + count);
+
+            SqlCommand avgCmd = new SqlCommand("SELECT AVG(AGE) FROM Students", conn);
+            int average = (int)avgCmd.ExecuteScalar();
+            Console.WriteLine("Total Students: " + averagexxaa);
+
+            conn.Close();
+        }
+    }
+}
