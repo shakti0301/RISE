78200879e655698bc0c99e8c2731ad40ec6f3895
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-25 16:42:48 +0530

    Create New DB for new project

--- SQL/DotnetCore/Queries.sql
+++ SQL/DotnetCore/Queries.sql
@@ -1,3 +1,4 @@
+---------EmployeeDB-------------
 CREATE DATABASE EmployeeDB
 
 USE EmployeeDB
@@ -49,3 +50,86 @@ AS
 BEGIN
     DELETE FROM Employees WHERE Id = @Id
 END
+
+
+
+
+-----------CompanyDB----------------
+CREATE DATABASE CompanyDB
+USE CompanyDB
+
+
+CREATE Table Departments(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Name VARCHAR(100)
+)
+
+CREATE TABLE Employees
+(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100),
+    Salary DECIMAL(18,2),
+    DepartmentId INT,
+
+    FOREIGN KEY (DepartmentId) REFERENCES Departments(Id)
+);
+
+INSERT INTO Departments (Name) VALUES ('IT'),('HR'),('Finance')
+
+INSERT INTO Employees (Name, Salary, DepartmentId) VALUES ('Shakti', 50000, 1),('Rohit', 60000, 2),( 'Anjali', 55000, 3)
+
+
+CREATE PROCEDURE GetEmployees
+AS
+BEGIN
+    SELECT E.Id, E.Name, E.Salary, D.Name AS DepartmentName, E.DepartmentId
+    FROM Employees E
+    JOIN Departments D ON E.DepartmentId = D.Id
+END
+
+
+CREATE PROCEDURE GetDepartments
+AS
+BEGIN
+    SELECT * FROM Departments
+END
+
+
+CREATE PROCEDURE AddEmployee
+    @Name NVARCHAR(100),
+    @Salary DECIMAL(18,2),
+    @DepartmentId INT
+AS
+BEGIN
+    INSERT INTO Employees (Name, Salary, DepartmentId)
+    VALUES (@Name, @Salary, @DepartmentId)
+END
+
+
+CREATE PROCEDURE UpdateEmployee
+    @Id INT,
+    @Name NVARCHAR(100),
+    @Salary DECIMAL(18,2),
+    @DepartmentId INT
+AS
+BEGIN
+    UPDATE Employees
+    SET Name = @Name, Salary = @Salary, DepartmentId = @DepartmentId
+    WHERE Id = @Id
+END
+
+
+CREATE PROCEDURE DeleteEmployee
+    @Id INT
+AS
+BEGIN
+    DELETE FROM Employees WHERE Id = @Id
+END
+
+CREATE PROCEDURE GetEmployeeById
+    @Id INT
+AS
+BEGIN
+    SELECT * FROM Employees WHERE Id = @Id
+END
+
