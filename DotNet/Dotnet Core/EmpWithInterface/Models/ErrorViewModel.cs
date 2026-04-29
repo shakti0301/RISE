ede4daa8d4ed5bbcd36a0fc159cda601b2ae8985
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-29 20:59:34 +0530

    Delete the app

--- DotNet/Dotnet Core/EmpWithInterface/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EmpWithInterface.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
